4b3a3f0739001fc33ba655417a70f6dc17d15310
h0za1fa <hozaifaco@gmail.com>
2025-09-17 21:49:01 +0500

    Refactor paint coverage calculation to use math.ceil for accurate paint requirement

--- paintcoverage.py
+++ paintcoverage.py
@@ -1,3 +1,5 @@
+import math
+
 wall_hieght=int(input('Enter hieght: '))
 wall_width=int(input('Enter width: '))
 coverage=5
@@ -5,9 +7,7 @@ coverage=5
 def paint_needed(hieght,width,coverage) -> int:
     area=hieght*width
     paint_req=round(area/coverage , 1)
-    paint_req_int=int(paint_req)
-    if paint_req > int(paint_req):
-        paint_req_int+=1
+    paint_req_int=math.ceil(paint_req)
     return paint_req_int
 
 print(f'Paint required is {paint_needed(wall_hieght,wall_width,coverage)}')
