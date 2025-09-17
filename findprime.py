3e605802d3579c30c99213cdf8aded5ed102aba3
h0za1fa <hozaifaco@gmail.com>
2025-09-17 22:09:58 +0500

    Add prime number checker script

---
+++ findprime.py
@@ -0,0 +1,14 @@
+
+num=int(input('Enter number: '))
+
+def prime(num):
+    for i in range (2,9):
+        if num in [2,3,5,7]:
+            status='prime'
+        elif num/i > int(num/i):
+            status='not prime'
+        else:
+            status='prime'
+    return status
+    
+print(f'{num} is {prime(num)}')
