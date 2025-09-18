aac3411a76d53ed17475aedc176cce9818cccfdf
h0za1fa <hozaifaco@gmail.com>
2025-09-18 23:49:09 +0500

    Fix Caesar cipher logic to handle wrap-around for character shifting and add logo display

--- ceaser cypher.py
+++ ceaser cypher.py
@@ -1,18 +1,21 @@
+from ceaserart import logo
+
 def ceaser(msg,letters,shift,choice):
     encrypted=''
     if choice == 'd':
             shift = -shift
     for alph in msg :
         ind = letters . index (alph)
-        encrypted += letters [ind+shift]
+        ind = (ind+shift)%len(letters) 
+        encrypted += letters [ind]
     return encrypted
 
+letters=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-letters=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ,' ','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ,' ']
-
+logo
 choice=input('encode (e) or decode (d) \n')
 msg=input('Enter meseasge \n')
 shift=int(input('Enter shift \n'))
 print(ceaser(msg,letters,shift,choice))
 
-# gttgnjx
+# gttgnjx gttgnjxj
