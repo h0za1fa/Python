6b8989cfc69fc062f4e891b5d0df3598619fe6ef
h0za1fa <hozaifaco@gmail.com>
2025-09-23 23:11:19 +0500

    coffee ooops!

--- oop-coffee-machine-premade-classes/main.py
+++ oop-coffee-machine-premade-classes/main.py
@@ -1,3 +1,23 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+is_on = True
+
+while is_on == True:
+    print(f'What would you like to order {menu.get_items()[:-1]}')
+    order=input('Place your order: ')
+    order_available=menu.find_drink(order)
+    if order == 'report':
+        coffee_maker.report()
+        money_machine.report()
+    elif not order_available: 
+        order_available
+    elif coffee_maker.is_resource_sufficient(order_available) and money_machine.make_payment(order_available.cost):
+        coffee_maker.make_coffee(order_available)
+        
+        
