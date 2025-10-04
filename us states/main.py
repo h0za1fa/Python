5110838a114de652735fe4f67fea86dae3bdb2e3
h0za1fa <hozaifaco@gmail.com>
2025-10-04 10:57:17 +0500

    list comp to us state game

--- us states/main.py
+++ us states/main.py
@@ -1,19 +1,19 @@
 import turtle as tr
 import pandas as pd
 
-states_data = pd.read_csv('50_states.csv')
+states_data = pd.read_csv('us states/50_states.csv')
 tim = tr.Turtle()
 tim.penup()
 tim.ht()
 screen = tr.Screen()
-image = 'blank_states_img.gif'
+image = 'us states/blank_states_img.gif'
 screen.addshape(image)
 tr.shape(image)
 total_ans = 0
 
 state_list = states_data['state'].to_list()
 guessed_states = []
-not_guessed_states = []
+# not_guessed_states = []
 
 
 game_on = True
@@ -22,10 +22,12 @@ while total_ans <= 50 :
     ans_state = screen.textinput(title = 'Enter state name', prompt = 'Write the name of a state').title()
     
     if ans_state == 'Exit':
-        for state in state_list :
-            if state not in guessed_states :
-                not_guessed_states.append(state)
+        # for state in state_list :
+        #     if state not in guessed_states :
+        #         not_guessed_states.append(state)
+        not_guessed_states = [state for state in state_list if state not in guessed_states]
         break
+    
     
     if ans_state in state_list and ans_state not in guessed_states :
     
@@ -38,5 +40,5 @@ while total_ans <= 50 :
         total_ans +=1
         
 states_to_learn = pd.DataFrame(not_guessed_states, columns = ['states'] )
-states_to_learn.to_csv('states_to_learn.csv')
+states_to_learn.to_csv('us states/states_to_learn.csv')
 screen.mainloop()
